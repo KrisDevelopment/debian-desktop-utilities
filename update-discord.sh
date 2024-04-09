773bb48a30fbe762ca2fd8e46b3b4bcbae75930d
Kris Development <35272196+KrisDevelopment@users.noreply.github.com>
2024-04-09 22:28:28 +0300

    Create update-discord.sh

---
+++ update-discord.sh
@@ -0,0 +1,6 @@
+sudo wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+# wait for download to finish
+sleep 2
+sudo dpkg -i discord.deb
+sudo rm discord.deb
+echo "Discord updated"
