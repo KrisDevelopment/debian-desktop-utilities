b78d7b31af7a8f4120c996bef002fd31d93abc2d
KrisDevelopment <hristo@revoltsoftware.com>
2025-03-18 01:59:01 +0200

    search in deb tools menu

--- deb-desktop-tools-gui.sh
+++ deb-desktop-tools-gui.sh
@@ -13,18 +13,33 @@ if ! command -v whiptail &> /dev/null; then
     fi
 fi
 
-# Array of menu options by querying the scripts in the directory
-options=("Quit")
+search_string=""
+
 script_dir_arg=$(dirname "$0")
-for script in "$script_dir_arg"/*.sh; do
-    options+=("$script")
-done
 
 # Define colors
 colors=("\e[1;91m" "\e[1;32m" "\e[1;33m" "\e[1;34m" "\e[1;35m" "\e[1;36m") # Red, Green, Yellow, Blue, Magenta, Cyan
 reset_color="\e[0m"
 
 while true; do
+
+    # Array of menu options by querying the scripts in the directory
+    options=("Quit")
+
+    if [ -z "$search_string" ]; then
+        options+=("Search")
+
+        for script in "$script_dir_arg"/*.sh; do
+            options+=("$script")
+        done
+    else
+        for script in "$script_dir_arg"/*.sh; do
+            if grep -q "$search_string" "$script"; then
+                options+=("$script")
+            fi
+        done
+    fi
+
     echo
     # Display menu with alternating colors
     for i in "${!options[@]}"; do
@@ -32,11 +47,27 @@ while true; do
         printf "%b%s %s%b\n" "$color" "$i:" "${options[$i]}" "$reset_color"
     done
 
+    is_searching=1
+    if [ -z "$search_string" ]; then
+        is_searching=0
+    fi
+    
     read -p "Enter option: " option
-    case $option in
-        0) break;;
-        *) bash "${options[$option]}";;
-    esac
+    if [ -z "$search_string" ]; then
+        
+        case $option in
+            0) break;;
+            1) read -p "Enter search string: " search_string;;
+            *) bash "${options[$option]}";;
+        esac
+    else
+        case $option in
+            0) break;;
+            *) bash "${options[$option]}";;
+        esac
+    fi
 
-    read -p "Press to continue"
+    if [ $is_searching -eq 1 ]; then
+        search_string=""
+    fi
 done
