f0c67676132b7a8a968c573b0a9b0d67800727ed
hristo_iv <hristo@revoltsoftware.com>
2025-03-22 22:33:29 +0200

    pipewire install fixes - added libraries to make bluetooth work

--- audio-select-pipewire.sh
+++ audio-select-pipewire.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing pipewire..."
-sudo apt install pipewire
+sudo apt install pipewire libspa-0.2-bluetooth pipewire-audio
 
 echo "Enabling pipewire..."
 systemctl --user disable --now pulseaudio 
